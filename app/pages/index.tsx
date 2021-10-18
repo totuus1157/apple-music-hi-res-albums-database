94af438daccce758e2382c61bfb25d73b0be44c8
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2021-10-18 18:43:21 +0900

    Use Footer component instead of footer tag

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -51,18 +51,7 @@ const Home: NextPage = () => {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 };
