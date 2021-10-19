5f71907e901bf6318dc4faa68b2c4674fc0cfa22
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2021-10-19 20:15:02 +0900

    Add Album component

---
+++ app/components/Album.tsx
@@ -0,0 +1,48 @@
+import { useState, useEffect, SetStateAction } from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "./fire";
+import Table from "react-bootstrap/Table";
+
+const db = firebase.firestore();
+
+export default function Album(): JSX.Element {
+  const mydata: SetStateAction<any[]> = [];
+  const [data, setData] = useState(mydata);
+
+  useEffect((): void => {
+    db.collection("users")
+      .doc("VhTtKxlWjDbmkq0qyzAc")
+      .collection("albums")
+      .get()
+      .then((snapshot): void => {
+        snapshot.forEach((document): void => {
+          console.log(document.id);
+          const doc = document.data();
+          mydata.push(
+            <tr key={document.id}>
+              <td>{doc.artist}</td>
+              <td>{doc.genre}</td>
+              <td>{doc.sampleRate}</td>
+              <td>{doc.title}</td>
+            </tr>
+          );
+        });
+        setData(mydata);
+      });
+  }, []);
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Artist</th>
+          <th>Genre</th>
+          <th>Sample rate</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody>{data}</tbody>
+    </Table>
+  );
+}
--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,10 +1,12 @@
 import Title from "./Title";
+import Album from "./Album";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Main(props: { title: any }): JSX.Element {
   return (
     <main>
       <Title title={props.title} />
+      <Album />
     </main>
   );
 }
