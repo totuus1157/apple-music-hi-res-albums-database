2fef6c44bcad8127806f70dd07b0e6cb94fbe5f9
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2021-11-09 11:35:15 +0900

    Add show Buttons component only when logged in

--- app/components/Main.tsx
+++ app/components/Main.tsx
@@ -1,13 +1,18 @@
 import Title from "./Title";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "./fire";
 import Buttons from "./Buttons";
 import Album from "./Album";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const auth = firebase.auth();
 
 export default function Main(props: { title: any }): JSX.Element {
   return (
     <main>
       <Title title={props.title} />
-      <Buttons />
+      {auth.currentUser ? <Buttons /> : " "}
       <Album />
     </main>
   );
