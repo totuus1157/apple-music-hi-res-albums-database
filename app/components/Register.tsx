42981bff52d8818947f29079d3d16ffffa49f062
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2021-11-26 11:34:07 +0900

    Add new data registration

--- app/components/Register.tsx
+++ app/components/Register.tsx
@@ -1,11 +1,65 @@
+import { useState, useEffect } from "react";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "./fire";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+
+const db = firebase.firestore();
+const auth = firebase.auth();
 
 export default function ModalWindow(props: {
   setShow: (arg0: boolean) => any;
   show: boolean;
 }): JSX.Element {
+  const [artist, setArtist] = useState("");
+  const [genre, setGenre] = useState("");
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [sampleRate, setSampleRate] = useState("");
+  const [composer, setComposer] = useState(null);
+
+  const onChangeArtist = (e) => {
+    setArtist(e.target.value);
+  };
+  const onChangeGenre = (e) => {
+    setGenre(e.target.value);
+  };
+  const onChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const onChangeUrl = (e) => {
+    setUrl(e.target.value);
+  };
+  const onChangeSampleRate = (e) => {
+    setSampleRate(e.target.value);
+  };
+  const onChangeComposer = (e) => {
+    setComposer(e.target.value);
+  };
+
+  const doAction = (e) => {
+    const ob = {
+      artist: artist,
+      genre: genre,
+      title: title,
+      url: url,
+      sampleRate: sampleRate,
+      composer: composer,
+    };
+    if (auth.currentUser != null) {
+      db.collection("users")
+        .doc(auth.currentUser.email!)
+        .collection("albums")
+        .add(ob)
+        .then((ref) => {
+          console.log("ref: ", ref);
+          handleClose();
+        });
+    }
+  };
+
   const handleClose = (): any => {
     return props.setShow(false);
   };
@@ -16,12 +70,39 @@ export default function ModalWindow(props: {
         <Modal.Header closeButton>
           <Modal.Title>Adding a New Album</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Hello World!</Modal.Body>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="form-group">
+              <Form.Label>Title:</Form.Label>
+              <Form.Control type="text" onChange={onChangeTitle} />
+            </Form.Group>
+            <Form.Group controlId="form-group">
+              <Form.Label>Artist:</Form.Label>
+              <Form.Control type="text" onChange={onChangeArtist} />
+            </Form.Group>
+            <Form.Group controlId="form-group">
+              <Form.Label>Genre:</Form.Label>
+              <Form.Control type="text" onChange={onChangeGenre} />
+            </Form.Group>
+            <Form.Group controlId="form-group">
+              <Form.Label>Composer:</Form.Label>
+              <Form.Control type="text" onChange={onChangeComposer} />
+            </Form.Group>
+            <Form.Group controlId="form-group">
+              <Form.Label>Sample Rate:</Form.Label>
+              <Form.Control type="text" onChange={onChangeSampleRate} />
+            </Form.Group>
+            <Form.Group controlId="form-group">
+              <Form.Label>URL:</Form.Label>
+              <Form.Control type="text" onChange={onChangeUrl} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" onClick={doAction}>
             Save Changes
           </Button>
         </Modal.Footer>
