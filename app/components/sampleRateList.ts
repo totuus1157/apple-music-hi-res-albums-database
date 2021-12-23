5ac9d5c611415059063e7b2594b868c5deed580f
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2021-12-23 18:10:00 +0900

    Fix typos

--- app/components/sampleRateList.ts
+++ app/components/sampleRateList.ts
@@ -1,8 +1,8 @@
-const sampleLateList = [
+const sampleRateList = [
   { id: 1, sampleRate: "88.2" },
   { id: 2, sampleRate: "96" },
   { id: 3, sampleRate: "176.4" },
   { id: 4, sampleRate: "192" },
 ];
 
-export default sampleLateList;
+export default sampleRateList;
