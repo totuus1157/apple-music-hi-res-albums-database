5c4539e785b72536bd03aa01d7955aca8d941790
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-01-29 18:58:16 +0900

    Fix footer at the bottom

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,13 +3,13 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-// import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const title = "Apple Music Hi-Res Album Database";
 
   return (
-    <div>
+    <div className={styles.container}>
       <Header title={title} />
       <Main title={title} />
       <Footer />
