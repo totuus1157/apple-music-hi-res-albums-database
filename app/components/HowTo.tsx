3a284454ad41033445d1951743d81b0248e3ac08
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-02-03 10:28:18 +0900

    Add instructions for entering non-Latin characters

--- app/components/HowTo.tsx
+++ app/components/HowTo.tsx
@@ -1,4 +1,6 @@
 import Modal from "react-bootstrap/Modal";
+import Card from "react-bootstrap/Card";
+import Accordion from "react-bootstrap/Accordion";
 import Button from "react-bootstrap/Button";
 
 export default function HowTo(props: {
@@ -14,7 +16,7 @@ export default function HowTo(props: {
         p {
           color: red;
         }
-      `}</style>
+        `}</style>
       <Modal show={props.show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>How to Register</Modal.Title>
@@ -36,6 +38,39 @@ export default function HowTo(props: {
               &quot;Genre&quot; (if &quot;Classical&quot; is not selected, the
               &quot;Composer&quot; text box will not be activated).
             </li>
+            <Accordion>
+              <Card>
+                <Card.Header>
+                  <Accordion.Toggle as={Button} variant="link" eventKey="0">About entering non-Latin names</Accordion.Toggle>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <Card.Text>
+                      Only the Latin alphabet can be used for this service. If
+                      the name of the artist, album, or composer you wish to
+                      register is not in Latin, please convert it to the Latin
+                      alphabet specified by the country or region to which you
+                      belong before entering it (basically, please follow the
+                      notation rules specified by the International Organization
+                      for Standardization).
+                    </Card.Text>
+                    <Card.Text>Major ISO Romanization Standards:</Card.Text>
+                    <ul>
+                      <li><a href={"https://ru.wikipedia.org/wiki/ISO_9"} target="_blank" rel="noopener noreferrer">ISO 9 - Cyrillic</a></li>
+                      <li><a href={"https://ar.wikipedia.org/wiki/%D8%A3%D9%8A%D8%B2%D9%88_233"} target="_blank" rel="noopener noreferrer">ISO 233 - Arabic</a></li>
+                      <li><a href={"https://he.wikipedia.org/wiki/ISO_259"} target="_blank" rel="noopener norefeerrer">ISO 259 - Hebrew</a></li>
+                      <li><a href={"https://el.wikipedia.org/wiki/ISO_843"} target="_blank" rel="noopener norefeerrer">ISO 843 - Greek</a></li>
+                      <li><a href={"https://ja.wikipedia.org/wiki/ISO_3602"} target="_blank" rel="noopener norefeerrer">ISO 3602 - Japanese</a></li>
+                      <li><a href={"https://zh.wikipedia.org/wiki/%E6%B1%89%E8%AF%AD%E6%8B%BC%E9%9F%B3"} target="_blank" rel="noopener norefeerrer">ISO 7098 - Chinese</a></li>
+                      <li><a href={"https://ka.wikipedia.org/wiki/ISO_9984"} target="_blank" rel="noopener norefeerrer">ISO 9984 - Georgian</a></li>
+                      <li><a href={"https://hy.wikipedia.org/wiki/%D5%80%D5%A1%D5%B5%D5%A5%D6%80%D5%A5%D5%B6_%D5%A1%D5%B5%D5%A2%D5%B8%D6%82%D5%A2%D5%A5%D5%B6%D5%AB_%D5%BC%D5%B8%D5%B4%D5%A1%D5%B6%D5%A1%D5%AF%D5%A1%D5%B6%D5%A1%D6%81%D5%B8%D6%82%D5%B4"} target="_blank" rel="noopener norefeerrer">ISO 9985 - Armenian</a></li>
+                      <li><a href={"https://th.wikipedia.org/wiki/ISO_11940"} target="_blank" rel="noopener norefeerrer">ISO 11940 - Thai</a></li>
+                      <li><a href={"https://hi.wikipedia.org/wiki/%E0%A4%86%E0%A4%88%E0%A4%8F%E0%A4%B8%E0%A4%93_%E0%A5%A7%E0%A5%AB%E0%A5%AF%E0%A5%A7%E0%A5%AF"} target="_blank" rel="noopener norefeerrer">ISO 15919 - Devanagari and related Indic scripts</a></li>
+                    </ul>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
             <li>
               Play the album and click on the wavy icon on the right side of the
               window where the song title is displayed, the sampling rate will
