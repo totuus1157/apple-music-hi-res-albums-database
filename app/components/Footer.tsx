b2439cac952fd138adc1930899a320a5ab8ee1d9
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-02-05 13:27:02 +0900

    Improve open links to Twitter in a new tab

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,8 +7,14 @@ export default function Footer(): JSX.Element {
     <footer className={styles.footer}>
       <p>
         Copyright (C) {year}{" "}
-        <a href="https://twitter.com/totuus_tweet">Totuus1157</a> All Rights
-        Reserved.
+        <a
+          href="https://twitter.com/totuus_tweet"
+          rel="external nofollow noopener noreferrer"
+          target="_blank"
+        >
+          Totuus1157
+        </a>{" "}
+        All Rights Reserved.
       </p>
     </footer>
   );
