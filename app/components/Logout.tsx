3b3a3a840924afddf26ec582ae43f4ed98e63a3f
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2022-04-08 11:09:41 +0900

    Implove display of logged-in user name

--- app/components/Logout.tsx
+++ app/components/Logout.tsx
@@ -2,9 +2,11 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "./fire";
 import Modal from "react-bootstrap/Modal";
+import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
 const auth = firebase.auth();
+const user = auth.currentUser;
 
 export default function Logout(props: {
   show: boolean;
@@ -17,24 +19,62 @@ export default function Logout(props: {
     props.setShow(false);
   };
 
+  const doDelete = () => {
+    if (user) {
+      user
+        .delete()
+        .then(() => {
+          props.setLoginState(false);
+          props.setShow(false);
+        })
+        .catch((error) => {
+          console.log("error: ", error);
+        });
+    }
+  };
+
   const handleClose = (): void => props.setShow(false);
 
   return (
-    <Modal show={props.show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Logout</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <p>Do you want to execute?</p>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Close
-        </Button>
-        <Button variant="info" onClick={doAction}>
-          Logout
-        </Button>
-      </Modal.Footer>
-    </Modal>
+    <>
+      <style jsx>{`
+        span.linkButton {
+          position: relative;
+          bottom: 2px;
+          right: 5px;
+        }
+      `}</style>
+
+      <Modal show={props.show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Logout</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            Do you want to execute{user !== null && `, ${user.displayName}`}?
+          </p>
+          <Card body>
+            To delete your account registration,
+            <span className="linkButton">
+              <Button variant="link" onClick={doDelete}>
+                click here.
+              </Button>
+            </span>
+            <br />
+            <span className="text-danger">
+              Your registered album data will not be deleted.
+            </span>
+          </Card>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="info" onClick={doAction}>
+            Logout
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
