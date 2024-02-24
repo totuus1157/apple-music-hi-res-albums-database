2d698b37c516beba56f8a1d0ba5b44a2ad57df28
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2024-02-24 16:14:19 +0900

    Deleted unnecessary import

--- app/pages/datatable.tsx
+++ app/pages/datatable.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Header from "components/Header";
 import Main from "components/Main";
-import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "styles/Home.module.css";
 
 const DataTable: NextPage = (): JSX.Element => {
