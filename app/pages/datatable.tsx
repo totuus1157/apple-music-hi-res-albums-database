72c6ee9e40e6ed5785e44bf011683a90794e9bda
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2024-03-18 16:15:54 +0900

    Don't use CSS Modules

--- app/pages/datatable.tsx
+++ app/pages/datatable.tsx
@@ -1,16 +1,15 @@
 import type { NextPage } from "next";
 import Header from "components/Header";
 import Main from "components/Main";
-import styles from "styles/Home.module.css";
 
 const DataTable: NextPage = (): JSX.Element => {
   const title = "Data table";
 
   return (
-    <div className={styles.container}>
+    <>
       <Header title={title} />
       <Main />
-    </div>
+    </>
   );
 };
 
