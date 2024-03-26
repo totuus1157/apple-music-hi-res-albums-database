e5dc84cfd711b9004b871b015cb3c22bc9a117c5
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2024-03-26 10:29:20 +0900

    Add setting to use Sign in with Apple using Auth.js

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { NextUIProvider } from "@nextui-org/react";
-import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <NextUIProvider>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </NextUIProvider>
   );
 }
+
 export default MyApp;
