2fc27da14d8d27368cc295e7347f849cf9d13e38
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2024-05-31 16:30:35 +0900

    Use Vercel Postgres and Auth0 instead of Firestore

--- app/components/EditTable.tsx
+++ app/components/EditTable.tsx
@@ -1,8 +1,5 @@
-import { useState, useEffect, SetStateAction } from "react";
-import firebase from "firebase/compat/app";
-import "firebase/compat/firestore";
-import "firebase/compat/auth";
-import "components/fire";
+import { useState, useEffect } from "react";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import {
   Table,
   TableHeader,
@@ -15,25 +12,20 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 
-const db = firebase.firestore();
-const auth = firebase.auth();
-
 type Props = {
   albumInfo: string;
   setAlbumInfo: (arg0: string) => void;
   onOpen: () => void;
   setModalContent: (arg0: string) => void;
   isOpen: boolean;
-  uid: string;
 };
 
 export default function EditTable(props: Props): JSX.Element {
-  const { albumInfo, setAlbumInfo, onOpen, setModalContent, isOpen, uid } =
-    props;
-
-  const tableRows: SetStateAction<any[]> = [];
-  const [data, setData] = useState(tableRows);
+  const { albumInfo, setAlbumInfo, onOpen, setModalContent, isOpen } = props;
+  const [data, setData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const { user, error, isLoading } = useUser();
 
   const handleShow = (e: any): void => {
     setAlbumInfo(e.currentTarget.value);
@@ -42,57 +34,62 @@ export default function EditTable(props: Props): JSX.Element {
   };
 
   useEffect((): void => {
-    if (auth.currentUser !== null) {
-      db.collection("users")
-        .doc(uid)
-        .collection("albums")
-        .get()
-        .then((snapshot): void => {
-          snapshot.forEach((document): void => {
-            const doc = document.data();
-            tableRows.push(
-              <TableRow key={document.id}>
-                <TableCell>{doc.artist}</TableCell>
-                <TableCell>{doc.genre}</TableCell>
-                <TableCell>{doc.composer}</TableCell>
-                <TableCell>{doc.sampleRate}</TableCell>
-                <TableCell>
-                  <a href={doc.url} target="_blank" rel="noopener noreferrer">
-                    {doc.title}
-                  </a>
-                </TableCell>
-                <TableCell style={{ border: "none" }}>
-                  <Button
-                    isIconOnly
-                    color="danger"
-                    size="sm"
-                    value={[
-                      document.id,
-                      doc.artist,
-                      doc.title,
-                      doc.genre,
-                      doc.composer,
-                      doc.sampleRate,
-                    ]}
-                    onClick={handleShow}
-                  >
-                    <FontAwesomeIcon icon={faTrashAlt} />
-                  </Button>
-                </TableCell>
-              </TableRow>,
-            );
-          });
-          setData(tableRows);
+    if (user) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            `/api/get-albums-by-registrant?registrantId=${user.sub}`,
+          );
+          const result = await response.json();
+          const albums = result.albums.rows.map((doc: any) => (
+            <TableRow key={doc.id}>
+              <TableCell>{doc.artist}</TableCell>
+              <TableCell>{doc.genre.join(", ")}</TableCell>
+              <TableCell>{doc.composer.join(", ")}</TableCell>
+              <TableCell>{doc.sample_rate}</TableCell>
+              <TableCell>
+                <a href={doc.url} target="_blank" rel="noopener noreferrer">
+                  {doc.title}
+                </a>
+              </TableCell>
+              <TableCell style={{ border: "none" }}>
+                <Button
+                  isIconOnly
+                  color="danger"
+                  size="sm"
+                  value={[
+                    doc.id,
+                    doc.artist,
+                    doc.title,
+                    doc.genre.join(", "),
+                    doc.composer.join(", "),
+                    doc.sample_rate,
+                  ]}
+                  onClick={handleShow}
+                >
+                  <FontAwesomeIcon icon={faTrashAlt} />
+                </Button>
+              </TableCell>
+            </TableRow>
+          ));
+          setData(albums);
           setIsLoaded(true);
-        });
-    } else {
-      tableRows.push(
-        <TableRow key="1">
-          <TableCell>Cannot retrieve data.</TableCell>
-        </TableRow>,
-      );
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+
+      fetchData();
     }
-  }, [isOpen]);
+  }, [user, isOpen]);
+
+  if (isLoading) {
+    return <p>Loading user...</p>;
+  }
+
+  if (error) {
+    return <p>Error loading user: {error.message}</p>;
+  }
 
   return (
     <>
---
+++ app/pages/api/get-albums-by-registrant.ts
@@ -0,0 +1,20 @@
+import { sql } from "@vercel/postgres";
+import { NextApiResponse, NextApiRequest } from "next";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+) {
+  try {
+    const registrantId = request.query.registrantId as string;
+    if (!registrantId) throw new Error("Registrant ID required");
+
+    const albums = await sql`
+      SELECT * FROM albums WHERE registrant_id = ${registrantId};
+    `;
+
+    return response.status(200).json({ albums });
+  } catch (error: any) {
+    return response.status(500).json({ error: error.message });
+  }
+}
