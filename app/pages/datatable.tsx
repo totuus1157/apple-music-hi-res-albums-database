34dc64e3ae539888e50b1e72f187847d88b8d8aa
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2024-06-20 13:56:43 +0900

    Change title props to direct embed

--- app/pages/datatable.tsx
+++ app/pages/datatable.tsx
@@ -3,11 +3,9 @@ import Header from "components/Header";
 import Main from "components/Main";
 
 const DataTable: NextPage = (): JSX.Element => {
-  const title = "Data table";
-
   return (
     <>
-      <Header title={title} />
+      <Header title="Data Table" />
       <Main />
     </>
   );
