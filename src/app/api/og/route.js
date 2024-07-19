e30d217b86d0ce6b36959a4525d26e8792c26d5e
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2024-07-19 15:43:52 +0900

    Add process to generate OGP images

---
+++ src/app/api/og/route.js
@@ -0,0 +1,40 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 50,
+          background: "linear-gradient(to bottom right, #e0e0e0, #ffffff)",
+          color: "#333",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: "50px",
+          textAlign: "center",
+          boxSizing: "border-box",
+        }}
+      >
+        <div
+          style={{ fontWeight: "bold", fontSize: "80px", marginBottom: "60px" }}
+        >
+          Apple Music Hi-Res Albums Database
+        </div>
+        <div style={{ fontSize: "38px" }}>
+          A List of Hi-Res Lossless Albums for All &quot;Apple and Music
+          Lovers&quot;
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
