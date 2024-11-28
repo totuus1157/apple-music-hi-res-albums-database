36a70837fcbc8159aaee4985583bc408fe712734
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2024-11-28 14:51:40 +0900

    Improve safety when using the Twitter API

--- src/pages/api/twitter/tweet.ts
+++ src/pages/api/twitter/tweet.ts
@@ -1,13 +1,5 @@
-import { errors } from "jose";
 import { NextApiRequest, NextApiResponse } from "next";
 import { TwitterApi } from "twitter-api-v2";
-
-const client = new TwitterApi({
-  appKey: process.env.TWITTER_API_KEY!,
-  appSecret: process.env.TWITTER_API_SECRET!,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN!,
-  accessSecret: process.env.TWITTER_ACCESS_SECRET!,
-});
 
 export default async function handler(
   req: NextApiRequest,
@@ -17,18 +9,45 @@ export default async function handler(
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  console.log("req.body: ", req.body);
-
-  const { tweetContent } = req.body;
-
-  if (!tweetContent) {
-    return res.status(400).json({ error: "Tweet content is required" });
+  // Validate environment variables
+  const {
+    TWITTER_API_KEY,
+    TWITTER_API_SECRET,
+    TWITTER_ACCESS_TOKEN,
+    TWITTER_ACCESS_SECRET,
+  } = process.env;
+  if (
+    !TWITTER_API_KEY ||
+    !TWITTER_API_SECRET ||
+    !TWITTER_ACCESS_TOKEN ||
+    !TWITTER_ACCESS_SECRET
+  ) {
+    return res.status(500).json({ error: "Server configuration error" });
   }
 
+  const client = new TwitterApi({
+    appKey: TWITTER_API_KEY,
+    appSecret: TWITTER_API_SECRET,
+    accessToken: TWITTER_ACCESS_TOKEN,
+    accessSecret: TWITTER_ACCESS_SECRET,
+  });
+
   try {
+    const { tweetContent }: { tweetContent: string } = req.body;
+
+    if (!tweetContent || typeof tweetContent !== "string") {
+      return res
+        .status(400)
+        .json({ error: "Tweet content is required and must be a string" });
+    }
+
     const twitterClient = client.readWrite;
     const tweet = await twitterClient.v2.tweet(tweetContent);
-    res.status(200).json({ message: "Tweet posted successfully", tweet });
+
+    res.status(200).json({
+      message: "Tweet posted successfully",
+      tweetId: tweet.data.id,
+    });
   } catch (err) {
     console.error("Error posting tweet", err);
     res.status(500).json({ error: "Failed to post tweet" });
