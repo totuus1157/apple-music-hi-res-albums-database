db7e9f06e659101ac6df4e030420f5e77c294e33
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2025-02-18 14:01:34 +0900

    Remove unnecessary files

--- src/components/sampleRateList.ts
+++
@@ -1,8 +0,0 @@
-const sampleRateList = [
-  { id: 1, sampleRate: "88.2" },
-  { id: 2, sampleRate: "96" },
-  { id: 3, sampleRate: "176.4" },
-  { id: 4, sampleRate: "192" },
-];
-
-export default sampleRateList;
