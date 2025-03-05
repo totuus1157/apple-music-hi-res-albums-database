7125ff674c298affd49a8fd2f99ee8fc3bda698c
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2025-03-05 18:08:41 +0900

    Make it possible for search engines to crawl linked pages (experimental setup)

--- src/app/datatable/layout.tsx
+++ src/app/datatable/layout.tsx
@@ -4,7 +4,7 @@ import { UserProvider } from "@auth0/nextjs-auth0/client";
 export const metadata: Metadata = {
   title: "Data Table",
   description: "Manage Hi-Res Lossless albums data.",
-  robots: { index: false, follow: false },
+  robots: { index: false, follow: true },
 };
 
 export default function DatatableLayout({
