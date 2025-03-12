8d76e62639097b765091a339c5dbc5cb7f9832d0
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2025-03-12 16:46:11 +0900

    Add Facebook Share Button

--- src/app/socialActionsBar.tsx
+++ src/app/socialActionsBar.tsx
@@ -2,12 +2,24 @@
 
 import { Link, Button } from "@heroui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
+import {
+  faSquareFacebook,
+  faSquareXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 export default function SocialActionsBar() {
   return (
     <div className="flex gap-4 mt-8 mb-2">
+      <Button
+        as={Link}
+        href="https://www.facebook.com/dialog/share?app_id=1094439279122110&href=https%3A%2F%2Fwww.applemusichiresalbumsdb.com%2F&hashtag=%23AppleMusicHiResAlbumsDB"
+        isExternal
+        size="lg"
+        className="bg-transparent text-black shadow-none hover:bg-transparent hover:shadow-none"
+      >
+        {<FontAwesomeIcon icon={faSquareFacebook} size="2xl" />}
+      </Button>
       <Button
         as={Link}
         href="https://twitter.com/intent/tweet?text=Apple%20Music%20Hi-Res%20Albums%20Database%0A%0A&url=https%3A%2F%2Fwww.applemusichiresalbumsdb.com&hashtags=ApplemusichiresalbumsDB,AppleMusic,HiRes"
