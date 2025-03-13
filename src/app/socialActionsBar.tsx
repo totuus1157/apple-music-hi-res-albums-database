941ed69dd6837da3109c21975edaf56ce4b294be
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2025-03-13 17:05:33 +0900

    Change the implementation of the link button to the map method

--- src/app/socialActionsBar.tsx
+++ src/app/socialActionsBar.tsx
@@ -8,38 +8,42 @@ import {
 } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
+const parameters = [
+  {
+    id: 1,
+    icon: faSquareFacebook,
+    href: "https://www.facebook.com/dialog/share?app_id=1094439279122110&href=https%3A%2F%2Fwww.applemusichiresalbumsdb.com%2F&hashtag=%23AppleMusicHiResAlbumsDB",
+  },
+  {
+    id: 2,
+    icon: faSquareXTwitter,
+    href: "https://twitter.com/intent/tweet?text=Apple%20Music%20Hi-Res%20Albums%20Database%0A%0A&url=https%3A%2F%2Fwww.applemusichiresalbumsdb.com%2F&hashtags=AppleMusicHiResAlbumsDB,AppleMusic,HiRes",
+  },
+  {
+    id: 3,
+    icon: faEnvelope,
+    href: "mailto:anzumaru_software@icloud.com",
+  },
+];
+
 export default function SocialActionsBar() {
   return (
     <div className="flex gap-4 mt-8 mb-2">
-      <Button
-        as={Link}
-        href="https://www.facebook.com/dialog/share?app_id=1094439279122110&href=https%3A%2F%2Fwww.applemusichiresalbumsdb.com%2F&hashtag=%23AppleMusicHiResAlbumsDB"
-        isExternal
-        size="lg"
-        disableAnimation
-        className="bg-transparent text-black shadow-none hover:bg-transparent hover:shadow-none"
-      >
-        {<FontAwesomeIcon icon={faSquareFacebook} size="2xl" />}
-      </Button>
-      <Button
-        as={Link}
-        href="https://twitter.com/intent/tweet?text=Apple%20Music%20Hi-Res%20Albums%20Database%0A%0A&url=https%3A%2F%2Fwww.applemusichiresalbumsdb.com%2F&hashtags=AppleMusicHiResAlbumsDB,AppleMusic,HiRes"
-        isExternal
-        size="lg"
-        disableAnimation
-        className="bg-transparent text-black shadow-none hover:bg-transparent hover:shadow-none"
-      >
-        {<FontAwesomeIcon icon={faSquareXTwitter} size="2xl" />}
-      </Button>
-      <Button
-        as={Link}
-        href="mailto:anzumaru_software@icloud.com"
-        size="lg"
-        disableAnimation
-        className="bg-transparent text-black shadow-none hover:bg-transparent hover:shadow-none"
-      >
-        {<FontAwesomeIcon icon={faEnvelope} size="2xl" />}
-      </Button>
+      {parameters.map((parameter): void => {
+        return (
+          <Button
+            key={parameter.id}
+            as={Link}
+            href={parameter.href}
+            isExternal
+            size="lg"
+            disableAnimation
+            className="bg-transparent text-black shadow-none hover:bg-transparent hover:shadow-none"
+          >
+            {<FontAwesomeIcon icon={parameter.icon} size="2xl" />}
+          </Button>
+        );
+      })}
     </div>
   );
 }
