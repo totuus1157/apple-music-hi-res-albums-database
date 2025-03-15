d279ba14f1431f7db571c9851bef7f1a7b9384da
totuus1157 <54227150+totuus1157@users.noreply.github.com>
2025-03-15 18:43:04 +0900

    Update 'Update Notice'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,6 +59,15 @@ const HomePage: NextPage = (): JSX.Element => {
           <h2 className="text-3xl font-semibold mb-6 text-green-800">
             Update Notice
           </h2>
+          <h3 className="text-xl font-semibold mb-2 text-green-800">
+            March 15, 2025
+          </h3>
+          <p className="text-left text-green-700 leading-7 mb-6">
+            We’ve added link buttons to our official Facebook 📘 and X 🐦
+            accounts, and new social media share buttons have been jazzed up in
+            the footer 🎉. We’re on a mission to boost our user base by 100x
+            🚀—so help us spread the word and join the fun!
+          </p>
 
           <h3 className="text-xl font-semibold mb-2 text-green-800">
             February 24, 2025
@@ -75,15 +84,6 @@ const HomePage: NextPage = (): JSX.Element => {
           </h3>
           <p className="text-left text-green-700 leading-7 mb-6">
             🎉 We have surpassed <strong>3000 registered albums</strong> in the
-            database! Thank you to all contributors for your support! Let&apos;s
-            keep discovering amazing music together! 🎶🌟
-          </p>
-
-          <h3 className="text-xl font-semibold mb-2 text-green-800">
-            December 23, 2024
-          </h3>
-          <p className="text-left text-green-700 leading-7 mb-6">
-            🎉 We have surpassed <strong>2000 registered albums</strong> in the
             database! Thank you to all contributors for your support! Let&apos;s
             keep discovering amazing music together! 🎶🌟
           </p>
